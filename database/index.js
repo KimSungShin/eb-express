0a99a5ab71d9dc33aea141c3e933d644a5157c6e
99856 <kimss323@naver.com>
2018-05-29 15:23:35 +0900

    mysql package.json 추가

--- database/index.js
+++ database/index.js
@@ -4,7 +4,7 @@
 
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.RDS_DATABASE, process.env.RDS_USERNAME, process.env.RDS_PASSWORD, {
+const sequelize = new Sequelize(process.env.RDS_DB_NAME, process.env.RDS_USERNAME, process.env.RDS_PASSWORD, {
 	host: process.env.RDS_HOSTNAME,
 	dialect: 'mysql',
 	operatorsAliases: false,
